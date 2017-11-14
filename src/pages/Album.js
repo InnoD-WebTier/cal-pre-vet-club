d6368d91aeda1640488ae86857d1684b7fdd7204
Sheryl Chang <sheryl.chang@berkeley.edu>
2017-11-13 20:13:30 -0800

    separated styling into different files and linked css to js

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Grid from '../Grid.js';
+import '../styles/album.css';
 
 const items = [
 	{ name: '2017-2018', url: 'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Samoyed-2.jpg' },
