44671c1777d031b8d6d44fd53be33a32dc1fd0cb
Sheryl Chang <sheryl.chang@berkeley.edu>
2017-11-13 20:17:40 -0800

    adding App.js and general styling files

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import Grid from '../Grid.js';
 
 // A base file for all your pages to come together.
 
+const items = [
+	{ url: 'http://www.dogbreedslist.info/uploads/allimg/dog-pictures/Samoyed-2.jpg' },
+	{ url: 'http://cdn.akc.org/Marketplace/Breeds/Pembroke_Welsh_Corgi_SERP.jpg' },
+	{ url: 'https://i.ytimg.com/vi/wRx3Uvcktm8/maxresdefault.jpg' }
+];
+
+const items1 = [
+	{ url: 'https://www.thelabradorsite.com/wp-content/uploads/2015/07/yellow-labrador-puppy-garden.jpg' }, 
+	{ url: 'https://cdn.marketplace.akc.org/media/45488/1456977861_4573_8635.jpg' },
+	{ url: 'https://cdn.marketplace.akc.org/media/45488/1456977861_4573_8635.jpg' }
+];
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        Hello World!
-      </div>
-    );
-  }
+		return(
+			<div>
+				<div className="title">Pumpkin Carving Social</div>
+				<Grid className="gallery__grid" urls={items} />
+				<Grid className="gallery__grid" urls={items1} />
+			</div>
+		)
+	}
 }
 
 export default App;
